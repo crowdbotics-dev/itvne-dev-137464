dbb4200e0574d70e7fc197e4420c5544ccf71440
crowdbotics-dev <engineering@crowdbotics.com>
2024-11-15 08:06:23 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Yhbcd
+
+admin.site.register(Yhbcd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Yhbcd(models.Model):
+    "Generated Model"
+    vbhaz = models.BigIntegerField()
+    reywui = models.BigIntegerField()
